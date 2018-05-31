ba4041a2d04277d1dec4d392cd9a38cb3e8c57ec
NicoNeureiter <nico.neureiter@gmail.com>
2018-05-31 17:04:32 +0200

    implemented tree in nexus file and write_locations function

--- src/beast_interface.py
+++ src/beast_interface.py
@@ -12,10 +12,15 @@ BEGIN DATA;
     FORMAT DATATYPE=standard SYMBOLS={symbols} MISSING=? GAP=-;
     MATRIX
 {data}\t;
-END;'''
+END;
+
+BEGIN TREES;
+      TREE original_tree = {tree};
+END;
+'''
 
 
-def write_nexus(features, path, fossils=None):
+def write_nexus(simulation, features, path, fossils=None):
     features = np.asarray(features, dtype=int)
     n_societies, n_features = features.shape
 
@@ -27,12 +32,34 @@ def write_nexus(features, path, fossils=None):
         line = '\t\t' + society_name + '\t' + ''.join(map(str, fs)) + '\n'
         data_str += line
 
+    tree = simulation.get_newick_tree()
+
     nexus_str = NEXUS_TEMPLATE.format(
         n_societies = n_societies,
         n_features = n_features,
         symbols = '01',
-        data = data_str
+        data = data_str,
+        tree = tree
     )
 
     with open(path, 'w') as nexus_file:
         nexus_file.write(nexus_str)
+
+
+LOCATIONS_TAMPLATE = 'traits\tx\ty\n{data}'
+
+def write_locations(locations, path):
+    print(locations.shape)
+
+    rows = []
+    for i, loc in enumerate(locations):
+        name = 'society_%i' % i
+        row = '%s\t%f\t%f' % (name, locations[i, 0], locations[i, 1])
+        rows.append(row)
+
+    data_str = '\n'.join(rows)
+
+    locations_str = LOCATIONS_TAMPLATE.format(data=data_str)
+
+    with open(path, 'w') as locations_file:
+        locations_file.write(locations_str)

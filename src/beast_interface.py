9b20d0081f7748bdfa392ff0e48af405681a3d54
NicoNeureiter <nico.neureiter@gmail.com>
2018-06-13 20:04:23 +0200

    implemented write_beast_xml to automate analysis pipeline (skip use of BEAUTI GUI)

--- src/beast_interface.py
+++ src/beast_interface.py
@@ -20,23 +20,27 @@ END;
 '''
 
 
-def write_nexus(simulation, features, path, fossils=None):
-    features = np.asarray(features, dtype=int)
-    n_societies, n_features = features.shape
-
+def write_nexus(simulation, path, fossils=None):
     data_str = ''
-    for i_society in range(n_societies):
-        society_name = 'society_%i' % i_society
-        fs = features[i_society]
 
-        line = '\t\t' + society_name + '\t' + ''.join(map(str, fs)) + '\n'
+    for i, state in enumerate(simulation.societies):
+        name = state.name
+        fs = state.featureState.features.astype(int)
+
+        line = '\t\t' + name + '\t' + ''.join(map(str, fs)) + '\n'
+        data_str += line
+
+    for i, state in enumerate(simulation.history):
+        name = 'h' + state.name
+        fs = '?' * simulation.n_features
+        line = '\t\t%s\t%s\n' % (name, fs)
         data_str += line
 
     tree = simulation.get_newick_tree()
 
     nexus_str = NEXUS_TEMPLATE.format(
-        n_societies = n_societies,
-        n_features = n_features,
+        n_societies = simulation.n_sites + len(simulation.history),
+        n_features = simulation.n_features,
         symbols = '01',
         data = data_str,
         tree = tree
@@ -48,13 +52,13 @@ def write_nexus(simulation, features, path, fossils=None):
 
 LOCATIONS_TAMPLATE = 'traits\tx\ty\n{data}'
 
-def write_locations(locations, path):
-    print(locations.shape)
-
+def write_locations(simulation, path):
     rows = []
-    for i, loc in enumerate(locations):
-        name = 'society_%i' % i
-        row = '%s\t%f\t%f' % (name, locations[i, 0], locations[i, 1])
+    for i, state in enumerate(simulation.societies):
+        name = state.name
+        loc = state.geoState.location
+
+        row = '%s\t%f\t%f' % (name, loc[0], loc[1])
         rows.append(row)
 
     data_str = '\n'.join(rows)
@@ -63,3 +67,51 @@ def write_locations(locations, path):
 
     with open(path, 'w') as locations_file:
         locations_file.write(locations_str)
+
+
+XML_TEMPLATE_PATH = 'data/templates/beast_xml_template.xml'
+FEATURES_TEMPLATE_PATH = 'data/templates/features_template.xml'
+LOCATION_TEMPLATE_PATH = 'data/templates/location_template.xml'
+
+
+def write_beast_xml(simulation, path):
+    with open(XML_TEMPLATE_PATH, 'r') as xml_template_file:
+        xml_template = xml_template_file.read()
+    with open(FEATURES_TEMPLATE_PATH, 'r') as features_template_file:
+        features_template = features_template_file.read()
+    with open(LOCATION_TEMPLATE_PATH, 'r') as locations_template_file:
+        location_template = locations_template_file.read()
+
+    locations_xml = ''
+    features_xml = ''
+
+    for i, state in enumerate(simulation.societies):
+        name = state.name
+
+        loc = state.geoState.location
+        locations_xml += location_template.format(id=name, x=loc[0], y=loc[1])
+
+        fs = state.featureState.features.astype(int)
+        fs_str = ''.join(map(str, fs))
+        features_xml += features_template.format(id=name, features=fs_str)
+
+    for i, state in enumerate(simulation.history):
+        name = 'h' + state.name
+
+        locations_xml += '\t\t<taxon id="{name}"/>\n'.format(name=name)
+
+        fs_str = '?' * simulation.n_features
+        features_xml += features_template.format(id=name, features=fs_str)
+
+    tree_xml = simulation.get_newick_tree()
+
+    with open(path, 'w') as beast_xml_file:
+        beast_xml_file.write(
+            xml_template.format(
+                locations=locations_xml,
+                features=features_xml,
+                tree=tree_xml,
+                ntax=simulation.n_sites,
+                nchar=simulation.n_features
+            )
+        )

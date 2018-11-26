7addafa8f339fb0bcb9faf7b7dd654af287da0fa
NicoNeureiter <nico.neureiter@gmail.com>
2018-11-26 15:25:32 +0100

    constant WIP

--- src/analyze_tree.py
+++ src/analyze_tree.py
@@ -6,72 +6,12 @@ from math import atan2
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize as ColorNormalize
 import geopandas as gpd
 
 from src.tree import Node
-from src.util import norm, normalize, dist, grey
-from src.plotting import circular_histogram
-from src.evaluation import check_root_in_hpd
-
-PINK = (0.95, 0.15, 0.8)
-TURQUOISE = (0, 0.8, 0.9)
-
-
-def extract_tree_line(nexus):
-    for line in nexus.split('\n'):
-        line = line.strip()
-        if line.startswith('tree '):
-            return line
-
-
-def extract_newick_from_nexus(nexus):
-    tree_line = extract_tree_line(nexus)
-    _, tree_name, _, _, newick = tree_line.split()
-
-    print('Extracted tree: %s' % tree_name)
-
-    return newick
-
-
-def plot_edge(parent, child, no_arrow=False, **kwargs_arrow):
-    x, y = parent.location
-    cx, cy = child.location
-    if x != cx or y != cy:
-        if no_arrow:
-            plt.plot([x, cx], [y, cy], c=kwargs_arrow['color'], alpha=kwargs_arrow['alpha'])
-        else:
-            plt.arrow(x, y, (cx-x), (cy-y), length_includes_head=True, **kwargs_arrow)
-
-
-def plot_tree(tree: Node, color_fun=None, alpha_fun=None, cmap=None,
-              cnorm=None, anorm=None, lw=0.1, color='k', alpha = 1., no_arrow=False):
-
-    if color_fun is not None:
-        if cmap is None:
-            cmap = plt.get_cmap('PiYG')
-            # cmap = plt.get_cmap('plasma')
-            # cmap = plt.get_cmap('viridis')
-
-        if color_fun is not None:
-            color_values = [color_fun(*e) for e in tree.iter_edges()]
-            if cnorm is None:
-                cnorm = ColorNormalize(vmin=min(color_values), vmax=max(color_values))
-
-        if alpha_fun is not None:
-            alphas = [alpha_fun(*e) for e in tree.iter_edges()]
-            if anorm is None:
-                anorm = ColorNormalize(vmin=min(alphas), vmax=max(alphas))
-
-    for edge in tree.iter_edges():
-        if color_fun is not None:
-            c = color_fun(*edge)
-            color = cmap(cnorm(c))
-
-        if alpha_fun is not None:
-            alpha = anorm(alpha_fun(*edge))
-
-        plot_edge(*edge, color=color, alpha=alpha, width=lw, lw=0, no_arrow=no_arrow)
+from src.util import norm, normalize, grey, extract_newick_from_nexus
+from src.plotting import circular_histogram, plot_hpd, plot_tree
+from src.config import PINK, TURQUOISE
 
 
 def get_edge_drift_agreement(parent, child):
@@ -92,6 +32,8 @@ def get_edge_diff_rate(parent, child):
 def get_edge_heights(parent, child):
     return (parent['height'] + child['height']) / 2.
 
+def get_old_edges(parent, child, threshold=250.):
+    return (parent['height'] + child['height']) / 2. > threshold
 
 def angle_to_vector(angle):
     return np.array([np.cos(angle), np.sin(angle)])
@@ -102,7 +44,7 @@ COLOR_1 = (0.1, 0.6, 0.75)
 COLOR_2 = (0.5, 0.1, 0.4)
 
 
-def plot_eary_late_drift(tree: Node):
+def plot_early_late_drift(tree: Node):
     halftime = tree['height'] / 2
 
     angles_early = []
@@ -132,6 +74,11 @@ def plot_eary_late_drift(tree: Node):
                             label='Step direction - later half of the expansion',
                             ax=ax)
 
+    ax.set_rticks([])
+    ax.grid(False)
+    plt.tight_layout(pad=0)
+    plt.legend()
+
     return ax
 
 
@@ -150,17 +97,22 @@ def plot_global_local_drift(tree: Node):
         lengths_from_root.append(norm(step_from_root))
         angles_from_root.append(atan2(step_from_root[1], step_from_root[0]))
 
-    ax = circular_histogram(angles_from_root, bins=N_BINS, normed=1, color=COLOR_1,
-                            weights=lengths_from_root,
-                            double_hist=True,
-                            label='Directions from root (global drift)'
-                            )
-    ax = circular_histogram(angles, bins=N_BINS, normed=1, color=COLOR_2,
+    # ax = circular_histogram(angles_from_root, bins=N_BINS, normed=1, color=COLOR_1,
+    #                         weights=lengths_from_root,
+    #                         double_hist=True,
+    #                         label='Directions from root'
+    #                         )
+    ax = circular_histogram(angles_from_root, bins=N_BINS, normed=1, color=COLOR_2,
                             weights=lengths,
                             double_hist=True,
-                            label='Directions from direct ancestor (local drift)',
-                            ax=ax
+                            # label='Directions from direct ancestor',
+                            label='Directions from root',
+                            # ax=ax
                             )
+    ax.set_rticks([])
+    ax.grid(False)
+    plt.tight_layout(pad=0)
+    plt.legend()
 
     return ax
 
@@ -173,7 +125,9 @@ def invert(fun):
 
 def flatten(fun, alpha):
     def flat_fun(*args, **kwargs):
-        return fun(*args, **kwargs) ** alpha
+        v = fun(*args, **kwargs)
+        return np.sign(v) * (abs(v) ** alpha)
+
     return flat_fun
 
 
@@ -182,87 +136,121 @@ if __name__ == '__main__':
     # FAMILY = 'ie'
 
     if FAMILY == 'bantu':
-        TREE_PATH = 'data/bantu_brownian/nowhere.tree'
+        TREE_PATH = 'data/bantu_withoutgroup_2/nowhere.tree'
         LOCATION_KEY = 'location'
-        XLIM = (2, 52)
-        YLIM = (-35, 15)
+        # XLIM = (2, 52)
+        # YLIM = (-35, 15)
+        XLIM = (-30, 60)
+        YLIM = (-35, 25)
         DRIFT_ANGLE = -0.5
         DRIFT_DIRECTION = angle_to_vector(DRIFT_ANGLE)
         swap_xy = False
         HOMELAND = np.array([6.5, 10.5])
+        LW = 0.1
+        HPD = 60
+
+        PLOT_DRIFT_LEGEND = False
+        PLOT_TREE = True
+        PLOT_HPD = True
+        PLOT_ROOT = True
+        PLOT_HOMELAND = True
+        PLOT_TIPS = False
 
     elif FAMILY == 'ie':
-        # TREE_PATH = 'data/ie/IE_2MCCs.tree'
-        # LOCATION_KEY = 'trait'
+        TREE_PATH = 'data/ie/IE_2MCCs.tree'
+        LOCATION_KEY = 'trait'
 
-        TREE_PATH = 'data/ie/nowhere.tree'
-        LOCATION_KEY = 'location'
+        # TREE_PATH = 'data/ie/nowhere.tree'
+        # LOCATION_KEY = 'location'
 
         swap_xy = True
-        XLIM = (-25, 120)
-        YLIM = (20, 80)
+        XLIM = (-30, 110)
+        YLIM = (0, 75)
         DRIFT_ANGLE = 2.8
         DRIFT_DIRECTION = angle_to_vector(DRIFT_ANGLE)
+        LW = 0.12
+        HPD = 80
 
+        PLOT_DRIFT_LEGEND = False
+        PLOT_TREE = True
+        PLOT_HPD = False
+        PLOT_ROOT = False
+        PLOT_HOMELAND = False
+        PLOT_TIPS = False
     else:
         raise ValueError()
 
     # cmap = plt.get_cmap('PiYG')
     # cmap = plt.get_cmap('RdYlGn')
+    # cmap = plt.get_cmap('inferno')
     # cmap = plt.get_cmap('plasma')
     cmap = plt.get_cmap('viridis')
+    # cmap = plt.get_cmap('gnuplot')
 
     # Plot the world map in the background.
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    # world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world = gpd.read_file('data/naturalearth_50m_wgs84.geojson')
+    # world = gpd.read_file('data/ne_50m_admin_0_countries.geojson')
+
+
+
+    # world = world.to_crs(epsg=3395)
+    # world = world.to_crs(epsg=3035)
+    # XLIM = (-4e6, 11e6)
+    # YLIM = (0e6, 11.5e6)
+    # XLIM = (-3e6, 9e6)
+    # YLIM = (3e6, 11.5e6)
     ax = world.plot(color=grey(.95), edgecolor=grey(0.7), lw=.4, )
+    # ax = world.plot(color=grey(.98), edgecolor=grey(0.), lw=.3, )
 
     # Load Tree
     with open(TREE_PATH, 'r') as tree_file:
         nexus_str = tree_file.read()
         newick_str = extract_newick_from_nexus(nexus_str)
         tree = Node.from_newick(newick_str, location_key=LOCATION_KEY, swap_xy=swap_xy)
-        # if LOCATION_KEY == 'trait':
-        #     tree = tree.get_subtree([0, 0, 0, 0])
+        # tree = tree.get_subtree([0, 0, 0, 0])
 
     # Plot Tree
-    plot_tree(tree, cmap=cmap,
-              color_fun=invert(get_edge_heights),
-              alpha_fun=None)
-    okcool = check_root_in_hpd(TREE_PATH, 80, root=HOMELAND[::-1],
-                               ax=ax, color=PINK, zorder=2)
-    print(okcool)
+    if PLOT_TREE:
+        plot_tree(tree, cmap=cmap,
+                  color_fun=flatten(invert(get_edge_heights), .7),
+                  # color_fun=get_edge_drift_agreement,
+                  # alpha_fun=flatten(get_edge_heights, .01),
+                  lw=LW)
 
-    # Plot root location
-    root = tree.location
-    print(root)
-    plt.scatter(HOMELAND[1], HOMELAND[0], marker='*', c=TURQUOISE, s=500, zorder=3)
-    plt.scatter(root[0], root[1], marker='*', c=PINK, s=500, zorder=3)
-    # plt.scatter(root[0], root[1], marker='*', c=(0.85, 0.7, 0.), s=800, zorder=2,
-    #             edgecolors=(0.35, 0.3, 0), lw=1.)
+    if PLOT_HPD:
+        okcool = plot_hpd(tree, HPD)  #, root=HOMELAND[::-1], color=PINK, zorder=2)
 
-    ############################################################################
-    # Plot Legend Compass
-    ############################################################################
+    if PLOT_ROOT:
+        root = tree.location
+        plt.scatter(root[0], root[1], marker='*', c=PINK, s=500, zorder=3)
+        # plt.scatter(root[0], root[1], marker='*', c=(0.85, 0.7, 0.), s=800, zorder=2,
+        #             edgecolors=(0.35, 0.3, 0), lw=1.)
+    if PLOT_HOMELAND:
+        plt.scatter(HOMELAND[1], HOMELAND[0], marker='*', c=TURQUOISE, s=500, zorder=3)
 
-    px, py = -3., 73.
-    # px, py = 8., -30.
-    r = 2.5
-    n_arrows = 10
-    arrow_angles = np.linspace(0,2 * np.pi, n_arrows, endpoint=False)
-    arrow_angles = (arrow_angles + DRIFT_ANGLE) % (2 * np.pi)
-    for angle in arrow_angles:
-        direction = angle_to_vector(angle)
-        c = cmap(0.5 + 0.5*direction.dot(DRIFT_DIRECTION))
-        plt.arrow(px, py, r * direction[0], r * direction[1], color=c,
-                  width = 0.05)
 
-    c0 = plt.Circle((px, py), 0.1 * r, facecolor='w', edgecolor='grey', lw=0.5)
-    c1 = plt.Circle((px, py), 1.12 * r, facecolor=(0, 0, 0, 0), edgecolor='grey', lw=0.5)
-    ax.add_artist(c0)
-    ax.add_artist(c1)
+    if PLOT_DRIFT_LEGEND:
+        # px, py = -3., 73.
+        px, py = 8., -30.
+        r = 2.5
+        n_arrows = 10
+        arrow_angles = np.linspace(0,2 * np.pi, n_arrows, endpoint=False)
+        arrow_angles = (arrow_angles + DRIFT_ANGLE) % (2 * np.pi)
+        for angle in arrow_angles:
+            direction = angle_to_vector(angle)
+            c = cmap(0.5 + 0.5*direction.dot(DRIFT_DIRECTION))
+            plt.arrow(px, py, r * direction[0], r * direction[1], color=c,
+                      width = 0.05)
 
-    # locs = np.array([leaf.location for leaf in tree.iter_leafs()])
-    # plt.scatter(*locs.T, c='k', s=9.)
+        c0 = plt.Circle((px, py), 0.1 * r, facecolor='w', edgecolor='grey', lw=0.5)
+        c1 = plt.Circle((px, py), 1.12 * r, facecolor=(0, 0, 0, 0), edgecolor='grey', lw=0.5)
+        ax.add_artist(c0)
+        ax.add_artist(c1)
+
+    if PLOT_TIPS:
+        locs = np.array([leaf.location for leaf in tree.iter_leafs()])
+        plt.scatter(*locs.T, c='k', s=9.)
 
     # Plot Settings
     ax.set_xlim(XLIM)

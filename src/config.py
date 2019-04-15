3ee039927563435bed5891cf9e4598b7a16977a4
NicoNeureiter <nico.neureiter@gmail.com>
2019-04-15 19:47:58 +0200

    some more color functionality (should be moved somewhere else)

--- src/config.py
+++ src/config.py
@@ -27,18 +27,24 @@ class Cycler(object):
         n = len(self.data)
         return self.data[i % n]
 
-COLORS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b',
-           '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
-           '#ffe999', '#802a66', '#008090'
-          ]
-COLORS_RGB = Cycler([hex_to_rgb(c) for c in COLORS])
 
+_COLORS = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+           '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf',
+           '#ffe999', '#802a66', '#058090']
+COLORS = Cycler(_COLORS)
+COLORS_RGB = Cycler([hex_to_rgb(c) for c in _COLORS])
+GREY_TONES = Cycler([(int(255*x),)*3 for x in np.linspace(0.7,0.95, 10)])
+
+def gamma_transform(rgb, gamma):
+    r,g,b = rgb
+    rgb = tuple(int(255*(v/255)**gamma) for v in (r,g,b))
+    return rgb
 
 
 # def to_hls(c):
 #     rgb = hex_to_rgb(c)
 #     return colorsys.rgb_to_hls(*rgb)
-
+#
 #
 # def hls_to_hex(h, l, s):
 #     r, g, b = colorsys.hls_to_rgb(h, l, s)

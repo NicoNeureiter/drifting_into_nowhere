fd4aef8d5efc1f0e060477a969872100e7ab4505
NicoNeureiter <nico.neureiter@gmail.com>
2019-07-24 15:12:28 +0200

    minor

--- src/experiments/experiment_status.py
+++ src/experiments/experiment_status.py
@@ -21,14 +21,24 @@ if __name__ == '__main__':
     x_name = X_NAME_BY_SIM_TYPE[simulation_type]
 
     for experiment in os.listdir(BASE_DIR):
+        # Construct path to results.csv
         experiment_path = os.path.join(BASE_DIR, experiment)
         results_path = os.path.join(experiment_path, RESULTS_FNAME)
-        print(results_path)
+
+        # Load results
         results = pd.read_csv(results_path)
 
+        # Skip if empty
+        if len(results) == 0:
+            continue
+
+        # Summarize results
         results = results.groupby(x_name)
         results_mean = results.mean()
         results_count = results.count()['i_repeat']
+
+        # Print summary
+        print(results_path)
         # print(results_mean.index)
         print(results_count)
         # print(results_mean['bias_norm'].round(decimals=2).values)

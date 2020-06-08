fefd33f44b936851cefa5044e65bed6228c11603
NicoNeureiter <nico.neureiter@gmail.com>
2020-06-08 19:35:14 +0200

    removed unused file

--- run_experiments.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-python3 -m src.experiments.experiment_evaluation rrw 0 &
-python3 -m src.experiments.experiment_evaluation rrw 200 &
-python3 -m src.experiments.experiment_evaluation rrw inf &
-python3 -m src.experiments.experiment_evaluation cdrw 0 &
-python3 -m src.experiments.experiment_evaluation cdrw 200 &
-python3 -m src.experiments.experiment_evaluation cdrw inf &
